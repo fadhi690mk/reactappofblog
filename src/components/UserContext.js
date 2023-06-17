80427e70ece6eca4e7d5d5cb3f3cb1af5e87cd17
fadhi690mk <88191826+fadhi690mk@users.noreply.github.com>
2023-06-18 00:38:10 +0530

    Update UserContext.js

--- src/components/UserContext.js
+++ src/components/UserContext.js
@@ -4,7 +4,7 @@ export const UserContext = createContext({});
 
 export function UserContextProvider({children}) {
     const [userInfo,setUserInfo] = useState({});
-    const REACT_APP_API_URL="http://localhost:4000/";
+    const REACT_APP_API_URL="https://mernappbackend3.onrender.com/";
 
     return (
         <UserContext.Provider value={{userInfo,setUserInfo,REACT_APP_API_URL}}>
